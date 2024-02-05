5866974c68f76e82b894904b3b02c209cc54b2e6
Daniel R <nzdanron@gmail.com>
2024-02-05 20:53:19 +1300

    feat: add `get_mouse_click_or_drag` function
    Almost identical to the `get_mouse_click` function, but this also captures dragging

--- src/input/mouse.rs
+++ src/input/mouse.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use crossterm::event::{MouseButton, read, Event, MouseEventKind};
+use crossterm::event::{read, Event, MouseButton, MouseEventKind};
 
 use crate::Grid;
 
@@ -19,9 +19,50 @@ use crate::Grid;
 pub fn get_mouse_click(grid: &Grid, button: MouseButton) -> io::Result<Option<(usize, usize)>> {
     if let Event::Mouse(event) = read()? {
         if event.kind == MouseEventKind::Down(button) {
+            // A cell is 2 columns wide
             let x = (event.column / 2) as usize;
             let y = event.row as usize;
-            return Ok(Some((x, y)))
+
+            // Clicks outside of the grid don't count
+            if x >= grid.width() && y >= grid.height() {
+                return Ok(None);
+            }
+
+            return Ok(Some((x, y)));
+        }
+    }
+    Ok(None)
+}
+
+/// Gets the x, y coordinates of a mouse click in relation to the grid.
+///
+/// The conversion from actual x, y to the pixel grid is automatic
+///
+/// # Parameters
+///
+/// - `grid` The grid to get the click on
+/// - `button` The mouse button to get the click of
+///
+/// # Returns
+///
+/// `None` if the click is outside the grid, otherwise a tuple with the format (x, y)
+pub fn get_mouse_click_or_drag(
+    grid: &Grid,
+    button: MouseButton,
+) -> io::Result<Option<(usize, usize)>> {
+    if let Event::Mouse(event) = read()? {
+        if event.kind == MouseEventKind::Down(button) || event.kind == MouseEventKind::Drag(button)
+        {
+            // A cell is 2 columns wide
+            let x = (event.column / 2) as usize;
+            let y = event.row as usize;
+
+            // Clicks outside of the grid don't count
+            if x >= grid.width() && y >= grid.height() {
+                return Ok(None);
+            }
+
+            return Ok(Some((x, y)));
         }
     }
     Ok(None)
